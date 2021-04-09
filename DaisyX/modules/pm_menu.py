991525c9e959afce444826b87b52e064f47e5a17
shiwabot <71938018+shiwabot@users.noreply.github.com>
2021-04-09 08:14:07 +0530

    Update pm_menu.py

--- DaisyX/modules/pm_menu.py
+++ DaisyX/modules/pm_menu.py
@@ -82,12 +82,12 @@ async def get_start_func(message, strings, edit=False):
     )
     buttons.add(
         InlineKeyboardButton(strings["btn_channel"], url="https://t.me/DaisyXUpdates"),
-        InlineKeyboardButton("👥 Support Grup", url="https://t.me/DaisyXUpdates"),
+        InlineKeyboardButton("👥 Support Grup", url="https://t.me/Anierosupport"),
     )
     buttons.add(
         InlineKeyboardButton(
-            "👸🏼 Add DaisyX to your group",
-            url=f"https://telegram.me/daisyxbot?startgroup=true",
+            "👸🏼 Add Amelia to your group",
+            url=f"https://telegram.me/missameliabot?startgroup=true",
         )
     )
     # Handle error when user click the button 2 or more times simultaneously
@@ -129,7 +129,7 @@ async def help_cmd(message, strings):
 async def help_cmd_g(message, strings):
     text = strings["btn_group_help"]
     button = InlineKeyboardMarkup().add(
-        InlineKeyboardButton(text=text, url="https://t.me/DaisyXBOT?start")
+        InlineKeyboardButton(text=text, url="https://t.me/MissAmeliabot?start")
     )
     await message.reply(strings["help_header"], reply_markup=button)
 
